797148588086a9ee67d529ea6bfc4aa69a49fcf8
urcop <kirillzama@list.ru>
2024-05-13 04:12:10 +0300

    feature: update container

--- internal/app/dependencies/container.go
+++ internal/app/dependencies/container.go
@@ -1,3 +1,11 @@
 package dependencies
 
-type Container struct{}
+import (
+	"github.com/SiriusServiceDesk/auth-service/internal/repository"
+	"github.com/SiriusServiceDesk/auth-service/internal/services"
+)
+
+type Container struct {
+	UserService services.UserService
+	Redis       repository.RedisRepository
+}
