49fdf08a561100dc3f2d0a7b94f614cf51faf146
urcop <kirillzama@list.ru>
2024-05-13 04:13:27 +0300

    feature: update application initializer

--- internal/app/application.go
+++ internal/app/application.go
@@ -3,7 +3,9 @@ package app
 import (
 	"github.com/SiriusServiceDesk/auth-service/internal/app/dependencies"
 	"github.com/SiriusServiceDesk/auth-service/internal/app/initializers"
+	"github.com/SiriusServiceDesk/auth-service/internal/config"
 	"github.com/SiriusServiceDesk/auth-service/internal/repository"
+	"github.com/SiriusServiceDesk/auth-service/internal/services"
 	"github.com/SiriusServiceDesk/auth-service/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
@@ -12,9 +14,17 @@ import (
 type Application struct{}
 
 func InitApplication(app *fiber.App) {
-	repository.NewUserRepository()
+	cfg := config.GetConfig()
 
-	container := &dependencies.Container{}
+	userRepository := repository.NewUserRepository()
+	userService := services.NewUserService(userRepository)
+
+	redisRepository := repository.NewRedisClient(cfg)
+
+	container := &dependencies.Container{
+		UserService: userService,
+		Redis:       redisRepository,
+	}
 
 	grpcListener, err := initializers.InitializeGRPCListener()
 	if err != nil {
