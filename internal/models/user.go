e7be308bd900bc33f10b5d4b4e9b67cd711e2396
urcop <kirillzama@list.ru>
2024-05-13 04:11:00 +0300

    feature: update user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,18 @@
 package models
 
+import "time"
+
 type User struct {
 	Id         string  `json:"id"`
 	Name       string  `json:"name"`
 	Surname    string  `json:"surname"`
-	SecondName *string `json:"SecondName"`
+	SecondName *string `json:"second_name"`
 	Password   string  `json:"-"`
-	Email      string  `json:"email"`
+	Email      string  `json:"email" gorm:"unique:true"`
+	IsVerified bool    `json:"is_verified"`
+	TelegramId string  `json:"telegram_id"`
+	PhotoUrl   string  `json:"photo_url"`
+
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
 }
