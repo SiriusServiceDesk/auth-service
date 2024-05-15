3bed245676f8405eb45fead19fd76bf984c649fd
urcop <kirillzama@list.ru>
2024-05-15 20:31:05 +0300

    feature: added cors to swagger

--- internal/web/swagger/controller.go
+++ internal/web/swagger/controller.go
@@ -2,6 +2,7 @@ package swagger
 
 import (
 	_ "github.com/SiriusServiceDesk/auth-service/api"
+	"github.com/SiriusServiceDesk/auth-service/internal/middleware"
 	"github.com/SiriusServiceDesk/auth-service/internal/web"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
@@ -16,5 +17,6 @@ func NewSwaggerController() *Controller {
 }
 
 func (c *Controller) DefineRouter(app *fiber.App) {
+	app.Use(middleware.SetupCORS())
 	app.Get("/swagger/*", swagger.HandlerDefault)
 }
